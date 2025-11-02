b299f73e17f1447375cbc8ece227ed0ddd2fac31
Slimey <blastslimeyt@gmail.com>
2025-11-02 13:36:38 +0100

    builder

--- worlds/pokemon_bw/data/version.py
+++ worlds/pokemon_bw/data/version.py
@@ -8,9 +8,10 @@ class VersionCompatibility(NamedTuple):
     ap_minimum: tuple[int, int, int]
 
 
-version: tuple[int, int, int] = (0, 3, 11)
+version: tuple[int, int, int] = (0, 3, 13)
 
 compatibility: dict[tuple[int, int, int], VersionCompatibility] = {
+    (0, 3, 13): VersionCompatibility((0, 3, 0), (0, 3, 13), (0, 3, 13), (0, 6, 3)),
     (0, 3, 12): VersionCompatibility((0, 3, 0), (0, 3, 12), (0, 3, 9), (0, 6, 3)),
     (0, 3, 11): VersionCompatibility((0, 3, 0), (0, 3, 9), (0, 3, 9), (0, 6, 3)),
     (0, 3, 10): VersionCompatibility((0, 3, 0), (0, 3, 9), (0, 3, 9), (0, 6, 3)),
@@ -49,30 +50,26 @@ if __name__ == "__main__":
     apworld = "pokemon_bw"
     dev_dir = "D:/Games/Archipelago/custom_worlds/dev/"
 
-    with io.BytesIO() as no_images_apworld_io:
-        with (zipfile.ZipFile(no_images_apworld_io, "w", zipfile.ZIP_DEFLATED, True, 9) as zipf,
-              zipfile.ZipFile(dev_dir+apworld+".apworld", 'w', zipfile.ZIP_DEFLATED, True, 9) as zipf2):
-            metadata = {
-                "game": "Pokemon Black and White",
-                "minimum_ap_version": "0.6.3",
-                "authors": ["BlastSlimey", "SparkyDaDoggo"],
-                "world_version": ".".join(str(i) for i in version)
-            }
-            zipf.writestr(os.path.join(apworld, "archipelago.json"), orjson.dumps(metadata))
-            zipf2.writestr(os.path.join(apworld, "archipelago.json"), orjson.dumps(metadata))
-            for root, dirs, files in os.walk("../"):
-                if "__pycache__" in root:
-                    continue
-                for file in files:
-                    zipf2.write(os.path.join(root, file),
-                                os.path.relpath(os.path.join(root, file),
-                                                "../../"))
-                if "images" in root and not root.endswith("images"):
-                    continue
-                for file in files:
-                    zipf.write(os.path.join(root, file),
-                               os.path.relpath(os.path.join(root, file),
-                                               "../../"))
-        no_images_apworld_io.flush()
-        with zipfile.ZipFile(dev_dir+apworld+"_without_maps.zip", 'w', zipfile.ZIP_DEFLATED, True, 9) as zipf:
-            zipf.writestr(apworld+".apworld", no_images_apworld_io.getvalue())
+    with (zipfile.ZipFile(dev_dir + apworld + "_without_maps.apworld", "w", zipfile.ZIP_DEFLATED, True, 9) as zipf,
+          zipfile.ZipFile(dev_dir + apworld + ".apworld", 'w', zipfile.ZIP_DEFLATED, True, 9) as zipf2):
+        metadata = {
+            "game": "Pokemon Black and White",
+            "minimum_ap_version": ".".join(str(i) for i in ap_minimum()),
+            "authors": ["BlastSlimey", "SparkyDaDoggo"],
+            "world_version": ".".join(str(i) for i in version)
+        }
+        zipf.writestr(os.path.join(apworld, "archipelago.json"), orjson.dumps(metadata))
+        zipf2.writestr(os.path.join(apworld, "archipelago.json"), orjson.dumps(metadata))
+        for root, dirs, files in os.walk("../"):
+            if "__pycache__" in root:
+                continue
+            for file in files:
+                zipf2.write(os.path.join(root, file),
+                            os.path.relpath(os.path.join(root, file),
+                                            "../../"))
+            if "images" in root and not root.endswith("images"):
+                continue
+            for file in files:
+                zipf.write(os.path.join(root, file),
+                           os.path.relpath(os.path.join(root, file),
+                                           "../../"))
