4113b87e805399468e4864d311cfd3b633c72e73
Slimey <blastslimeyt@gmail.com>
2026-06-23 01:34:21 +0200

    filling some more funny dialog

--- worlds/pokemon_bw/data/text/funny_dialog.py
+++ worlds/pokemon_bw/data/text/funny_dialog.py
@@ -7,67 +7,110 @@ table: list[TextData] = [
                                                 "But as soon as they stepped out of[NextLine]"
                                                 "the gate, they were suddenly sitting[Scroll][NextLine]"
                                                 "on an unfamiliar bicycle![End][NextLine]"
-                                                "What a strange world we live in...[Terminate]"),
+                                                "What a strange world we live in..."),
     TextData("FlamelStones", "system", 236, 0, 547, "This level 100 EV Trained Fluffy Fluffer[NextLine]"
                                                     "will Fluff you up, on Arceus, you better[NextLine]"
-                                                    "watch your back. #GrassGangRiseUp![Terminate]"),
+                                                    "watch your back. #GrassGangRiseUp!"),
+    TextData("Eevee658", "system", 189, 0, 0, "I hate shorts. They’re uncomfortable[NextLine]and hard to wear.[End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 21, "N: [c_be09_#1_1]"
                                                          "Ya know, someone wanted me to do[NextLine]"
-                                                         "something... that I cannot do.[c_be09_#1_0][End][Terminate]"),
+                                                         "something... that I cannot do.[c_be09_#1_0][End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 22, "[c_be09_#1_1]"
-                                                         "You wanna know what?[c_be09_#1_0][End][Terminate]"),
+                                                         "You wanna know what?[c_be09_#1_0][End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 23, "[c_be09_#1_1]"
-                                                         "They wanted me to...[c_be09_#1_0][End][Terminate]"),
+                                                         "They wanted me to...[c_be09_#1_0][End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 24, "[c_be09_#1_1]"
-                                                         "...recite the entire Bee Movie script.[c_be09_#1_0]"
-                                                         "[End][Terminate]"),
+                                                         "...recite the entire Bee Movie script.[c_be09_#1_0][End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 25, "[c_be09_#1_1]"
                                                          "Wait, you don't know why I[NextLine]"
-                                                         "can't do that?[c_be09_#1_0][End][Terminate]"),
+                                                         "can't do that?[c_be09_#1_0][End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 26, "[c_be09_#1_1]"
                                                          "It's really simple, but people[NextLine]"
-                                                         "tend to forget it.[c_be09_#1_0][End][Terminate]"),
+                                                         "tend to forget it.[c_be09_#1_0][End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 27, "[c_be09_#1_1]"
                                                          "If I were to recite copyrighted material,[NextLine]"
                                                          "which requires that material to be[Scroll][NextLine]"
                                                          "included in the apworld, this game would[Scroll][NextLine]"
                                                          "get kicked out of the Archipelago Discord[Scroll][NextLine]"
-                                                         "Server in an instant.[c_be09_#1_0][End][Terminate]"),
+                                                         "Server in an instant.[c_be09_#1_0][End]"),
     TextData("Serterd/BlastSlimey", "story", 112, 0, 28, "[c_be09_#1_1]"
-                                                         "So a meta joke it is.[c_be09_#1_0][End][Terminate]"),
+                                                         "So a meta joke it is.[c_be09_#1_0][End]"),
     TextData("Serterd", "story", 351, 0, 19, "Nett hier.[End][NextLine]"
                                              "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                             "Württemberg?[Terminate]"),
+                                             "Württemberg?"),
     TextData("Serterd", "story", 358, 0, 14, "Nett hier.[End][NextLine]"
                                              "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                             "Württemberg?[Terminate]"),
+                                             "Württemberg?"),
     TextData("Serterd", "story", 363, 0, 5, "Nett hier.[End][NextLine]"
                                             "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                            "Württemberg?[Terminate]"),
+                                            "Württemberg?"),
     TextData("Serterd", "story", 377, 0, 20, "Nett hier.[End][NextLine]"
                                              "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                             "Württemberg?[Terminate]"),
+                                             "Württemberg?"),
     TextData("Serterd", "story", 387, 0, 18, "Nett hier.[End][NextLine]"
                                              "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                             "Württemberg?[Terminate]"),
+                                             "Württemberg?"),
     TextData("Serterd", "story", 400, 0, 2, "Nett hier.[End][NextLine]"
                                             "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                            "Württemberg?[Terminate]"),
+                                            "Württemberg?"),
     TextData("Serterd", "story", 406, 0, 13, "Nett hier.[End][NextLine]"
                                              "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                             "Württemberg?[Terminate]"),
+                                             "Württemberg?"),
     TextData("Serterd", "story", 415, 0, 3, "Nett hier.[End][NextLine]"
                                             "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                            "Württemberg?[Terminate]"),
+                                            "Württemberg?"),
     TextData("Serterd", "story", 247, 0, 17, "Nett hier.[End][NextLine]"
                                              "Aber waren sie schon mal in Baden-[Scroll][NextLine]"
-                                             "Württemberg?[Terminate]"),
+                                             "Württemberg?"),
     TextData("Kuchiri", "system", 236, 0, 384, "Oh, so you allowed yourself in the[NextLine]"
                                                "yaml to obtain this pokémon. We[NextLine]"
-                                               "won't tell anyone ;)[Terminate]"),
+                                               "won't tell anyone ;)"),
     TextData("ZoZoBab", "system", 236, 0, 549, "You caught a Zoey![NextLine]"
                                                "I love you Odin![NextLine]"
-                                               "Viva la Spongegang![Terminate]"),
+                                               "Viva la Spongegang!"),
+    TextData("Deltakip", "system", 189, 0, 3, "Dodododward Gogogoglove[NextLine]or Zubababancho Gagagacoat?[End]"),
+    TextData("BlastSlimey", "story", 160, 0, 0, "The developers of this game have no[NextLine]"
+                                                "faith in you and thought, that you might[Scroll][NextLine]"
+                                                "not be able to beat the first gym.[End]"),
+    TextData("BlastSlimey", "story", 160, 0, 1, "But hey, it's a free encounter for your[NextLine]"
+                                                "Nuzlocke!"),
+    TextData("BlastSlimey", "story", 160, 0, 9, "Please take better care of it than[NextLine]"
+                                                "Slimey did with their Kangaskhan."),
+    TextData("BlastSlimey", "story", 160, 0, 3, "Come on, take it!"),
+    TextData("BlastSlimey", "story", 442, 0, 0, "I let mine 5 year old son play this game[NextLine]"
+                                                "because he sad he liked game and want[Scroll][NextLine]"
+                                                "very much so I buy game for him but[Scroll][NextLine]"
+                                                "little later he come to me and say[Scroll][NextLine]"
+                                                "\"daddy this game much scary!!\"[End][NextLine]"
+                                                "I look at game and it say:[NextLine]"
+                                                "\"ZOMBIE EAT YOU BRAIN!!!\"[Scroll][NextLine]"
+                                                "and me was shock. how can this game be[Scroll][NextLine]"
+                                                "allow for childs? this game has blood[Scroll][NextLine]"
+                                                "and violins, zombie eat your vegetables!![Scroll][NextLine]"
+                                                "zombie may no eat vegetable, my son eat[Scroll][NextLine]"
+                                                "vegetable!! he is now life shock and he[Scroll][NextLine]"
+                                                "have to take pills everyday!! tanks so[Scroll][NextLine]"
+                                                "very much electric art!!! this game much[Scroll][NextLine]"
+                                                "bad and scary, I rate 16 and over!! not[Scroll][NextLine]"
+                                                "play this game, this very[End][NextLine]"
+                                                "innapropait!!! u has been warning"),
+    TextData("BlastSlimey", "story", 442, 0, 2, "i am now life shock and i have to take[Scroll][NextLine]"
+                                                "pills everyday!!"),
+    TextData("BlastSlimey", "story", 436, 0, 27, "[c_be09_#1_1]You know, my father left 17 years ago[NextLine]"
+                                                 "to buy milk from the grocery store.[End][NextLine]"
+                                                 "Now he's suddenly back and gives public[NextLine]"
+                                                 "speeches. I fear that he joined a cult -[Scroll][NextLine]"
+                                                 "or even worse, he created one![c_be09_#1_0][End]"),
+    TextData("BlastSlimey", "story", 436, 0, 28, "[c_be09_#1_1]But he convinced me to help him in his[NextLine]"
+                                                 "mission.[End][NextLine]"
+                                                 "...[End][NextLine]"
+                                                 "...this guy, that just held a speech in[NextLine]"
+                                                 "front of us, being my father? Uhm, n-no,[Scroll][NextLine]"
+                                                 "I don't know this man! My father is[Scroll][NextLine]"
+                                                 "currently at home and plans to...[Scroll][NextLine]"
+                                                 "strengthen the bond between trainer[Scroll][NextLine]"
+                                                 "and Pokémon![End][NextLine]"
+                                                 "You know what, let's battle![c_be09_#1_0][End]"),
 ]
 """
     TextData("AAA", "system", xxx, 0, xxx, "AAA"),
