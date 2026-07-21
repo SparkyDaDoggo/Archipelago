69ab41e779ca3edcd3efa7b1467dbcf9872f5348
Slimey <blastslimeyt@gmail.com>
2026-07-21 17:45:25 +0200

    optimized stats writing

--- worlds/pokemon_bw/patch/procedures/write_stats.py
+++ worlds/pokemon_bw/patch/procedures/write_stats.py
@@ -15,37 +15,25 @@ def write_patch(bw_patch_instance: "PokemonBWPatch", opened_zipfile: zipfile.Zip
     for species, data in bw_patch_instance.world.species_entries.items():
         if data.form and not data.is_custom_form:
             continue
-        byt = bytearray(24)
 
-        if data.write & 0b100:
-            byt[:6] = (data.base_hp, data.base_attack, data.base_defense,
-                       data.base_speed, data.base_sp_attack, data.base_sp_defense)
+        byt = bytearray(data.write.to_bytes(2, "little"))
 
         if data.write & 0b1:
-            byt[6] = data.evolution_stage
-
+            byt.append(data.evolution_stage)
+        if data.write & 0b100:
+            byt.extend(data.base_stats)
         if data.write & 0b1000:
-            byt[7] = data.catch_rate
-
+            byt.append(data.catch_rate)
         if data.write & 0b100000:
-            byt[8] = by_name[data.type_1]
-            byt[9] = by_name[data.type_2]
-        else:
-            byt[8] = 0xff
-            byt[9] = 0xff
-
+            byt.append(by_name[data.types[0]])
+            byt.append(by_name[data.types[1]])
         if data.write & 0b1000000:
             flags = 0
             for tm in data.tm_hm_moves.tm_hm_moves:
                 flags |= 1 << tm_hm[tm].index
-            byt[10:23] = flags.to_bytes(13, "little")
-        else:
-            byt[22] = 0x80
-
+            byt.extend(flags.to_bytes(13, "little"))
         if data.write & 0b10000000:
-            byt[23] = data.exp_curve
-        else:
-            byt[23] = 0xff
+            byt.append(data.exp_curve)
 
         if data.write & 0b11101101:
             opened_zipfile.writestr(f"stats/{max(data.dex_number, data.custom_form_file)}", bytes(byt))
@@ -55,21 +43,40 @@ def patch(rom: NintendoDSRom, world_package: str, bw_patch_instance: "PokemonBWP
           files_dump: dict[str, bytes | bytearray]) -> None:
 
     narc = NARC(rom.getFileByName("a/0/1/6"))
+    loaded: bytes
+
+    def read_one() -> int:
+        nonlocal loaded
+        _val = loaded[0]
+        loaded = loaded[1:]
+        return _val
+
+    def read(_count: int) -> bytes:
+        nonlocal loaded
+        _val = loaded[:_count]
+        loaded = loaded[_count:]
+        return _val
 
     for i in range(1, 668):
         if f"stats/{i}" in bw_patch_instance.files:
             byt = bytearray(narc.files[i])
             loaded = bw_patch_instance.files[f"stats/{i}"]
 
-            for j in range(6):
-                byt[j] = loaded[j] or byt[j]
-            byt[9] = loaded[6] or byt[9]
-            byt[8] = loaded[7] or byt[8]
-            byt[6] = loaded[8] if loaded[8] != 0xff else byt[6]
-            byt[7] = loaded[9] if loaded[9] != 0xff else byt[7]
-            if not loaded[22] & 0x80:
-                byt[0x28:0x35] = loaded[10:23]
-            byt[21] = loaded[23] if loaded[23] != 0xff else byt[21]
+            flags = int.from_bytes(read(2), "little")
+            if flags & 0b1:
+                byt[9] = read_one()
+            if flags & 0b100:
+                for j in range(6):
+                    byt[j] = read_one() or byt[j]
+            if flags & 0b1000:
+                byt[8] = read_one()
+            if flags & 0b100000:
+                byt[6] = read_one()
+                byt[7] = read_one()
+            if flags & 0b1000000:
+                byt[0x28:0x35] = read(13)
+            if flags & 0b10000000:
+                byt[21] = read_one()
 
             narc.files[i] = bytes(byt)
             files_dump[f"a016/{i}"] = narc.files[i]
