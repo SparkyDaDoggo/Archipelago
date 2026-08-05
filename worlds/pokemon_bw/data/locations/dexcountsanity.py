0f6f2f4c103bd31269a91bf44cae7bb46b5add03
Slimey <blastslimeyt@gmail.com>
2026-08-05 02:55:10 +0200

    forgot dexcountsanity loc table

---
+++ worlds/pokemon_bw/data/locations/dexcountsanity.py
@@ -0,0 +1,5 @@
+# Other ...countsanity variations will also be here once implemented
+
+location_table: dict[str, int] = {
+    f"Pokédex - Catch {count} Pokémon": count for count in range(1, 650)
+}
